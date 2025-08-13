13b53ab5933bd5ff627495f12f04b6460a994739
Christelle <christelle.rolin@outlook.fr>
2025-08-13 22:27:34 +0200

    Sidebar fixe
    import images pub et autres corrections

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
 
 }
+
