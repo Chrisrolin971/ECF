bc61184d210beaf3818df53532e1a51a681f9746
Christelle <christelle.rolin@outlook.fr>
2025-08-15 14:09:06 +0200

    route 8 : inscription v2

--- src/app/pages/inscription/inscription.component.ts
+++ src/app/pages/inscription/inscription.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './inscription.component.html',
-  styleUrl: './inscription.component.scss'
+  styleUrls: ['./inscription.component.scss']
 })
 export class InscriptionComponent {
+  nom = '';
+  email = '';
+  motDePasse = '';
 
+  onSubmit(): void {
+    console.log('Nom:', this.nom);
+    console.log('Email:', this.email);
+    console.log('Mot de passe:', this.motDePasse);
+    // Tu peux ajouter ici l’appel à ton service d’inscription
+  }
 }
