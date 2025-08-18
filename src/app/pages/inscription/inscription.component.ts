6e36fba5d83d5cdfa0eb8840285a675f2cb97aa1
Christelle <christelle.rolin@outlook.fr>
2025-08-18 23:16:42 +0200

    route 8 : inscription v3

--- src/app/pages/inscription/inscription.component.ts
+++ src/app/pages/inscription/inscription.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {EMPTY_SUBSCRIPTION} from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-inscription',
@@ -11,13 +12,44 @@ import { FormsModule } from '@angular/forms';
 })
 export class InscriptionComponent {
   nom = '';
+  prenom = '';
+  pseudo = '';
   email = '';
   motDePasse = '';
+  confMotdePasse = '';
+  messageErreur = '';
+
 
   onSubmit(): void {
+
+    if (this.motDePasse !== this.confMotdePasse) {
+      this.messageErreur = 'Les mots de passe ne correspondent pas.';
+      return;
+    }
+
+    if (this.nom == '' || this.prenom == '' ||
+    this.pseudo == '' || this.email == '' ||
+    this.motDePasse == '' || this.confMotdePasse == '') {
+      this.messageErreur = 'Tous les champs sont obligatoires.';
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.email.trim())) {
+      this.messageErreur = 'Le format de l’adresse email est invalide.<br> ' +
+        'Elle doit être sous forme : exemple@domaine.com';
+      return;
+    }
+
+    // Si tout va bien, on vide le message d’erreur
+    this.messageErreur = '';
+
     console.log('Nom:', this.nom);
+    console.log('Prénom:', this.prenom);
+    console.log('Pseudo:', this.pseudo);
     console.log('Email:', this.email);
     console.log('Mot de passe:', this.motDePasse);
+    console.log('Confirm Mot de passe:', this.confMotdePasse);
     // Tu peux ajouter ici l’appel à ton service d’inscription
   }
 }
