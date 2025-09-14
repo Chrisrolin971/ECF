e865ee056d11593078503f24ad591bb532904fe4
Christelle <christelle.rolin@outlook.fr>
2025-09-14 19:40:51 +0200

    route 1 : home
    liaison à la base ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { HomeService, FilmCard } from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -9,31 +10,15 @@ import {CommonModule} from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+export class HomeComponent implements OnInit {
+  nouveautes: FilmCard[] = [];
+  affiches: FilmCard[] = [];
+  private readonly homeService = inject(HomeService);
 
-export class HomeComponent {
-  nouveautes = [
-    { titre: 'Les 4 Fantastiques', image: 'assets/films/4fantastiques.jpg' },
-    { titre: 'Cannes', image: 'assets/films/cannes.jpg' },
-    { titre: 'Dragon', image: 'assets/films/dragons.jpg' },
-    { titre: 'Flow', image: 'assets/films/flow.jpg' },
-  ];
-
-  affiches = [
-    { titre: 'Les 4 Fantastiques', image: 'assets/films/4fantastiques.jpg' },
-    { titre: 'Back in Action', image: 'assets/films/BackInAction.jpg' },
-    { titre: 'Cannes', image: 'assets/films/cannes.jpg' },
-    { titre: 'Dora', image: 'assets/films/dora.jpg' },
-    { titre: 'Dragon', image: 'assets/films/dragons.jpg' },
-    { titre: 'Elio', image: 'assets/films/elio.jpg' },
-    { titre: 'Empereur de Paris', image: 'assets/films/EmpereurDeParis.jpg' },
-    { titre: 'Evanouis', image: 'assets/films/evanouis.jpg' },
-    { titre: 'Falcon Express', image: 'assets/films/falconExpress.jpg' },
-    { titre: 'Flow', image: 'assets/films/flow.jpg' },
-    { titre: 'Grand Déplacement', image: 'assets/films/GrandDeplacement.jpg' },
-    { titre: 'Lilo & Stitch', image: 'assets/films/LiloStitch.jpg' },
-    { titre: 'Monde Jurassic', image: 'assets/films/mondeJurassic.jpg' },
-    { titre: 'Superman', image: 'assets/films/superman.jpg' },
-    { titre: 'Together', image: 'assets/films/together.png' },
-    { titre: 'Les Tuches', image: 'assets/films/tuche.jpg' },
-  ];
+  ngOnInit(): void {
+    this.homeService.getFilms().subscribe(data => {
+      this.nouveautes = data.nouveautes;
+      this.affiches = data.affiches;
+    });
+  }
 }
---
+++ src/app/pages/home/home.service.ts
@@ -0,0 +1,19 @@
+﻿import {inject, Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface FilmCard {
+  id: number;
+  titre: string;
+  image: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class HomeService {
+  private readonly apiUrl = 'http://ecf.local/backend/api/home.php';
+  private readonly http = inject(HttpClient);
+
+  getFilms(): Observable<{ nouveautes: FilmCard[]; affiches: FilmCard[] }> {
+    return this.http.get<{ nouveautes: FilmCard[]; affiches: FilmCard[] }>(this.apiUrl);
+  }
+}
