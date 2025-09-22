b2615a7e564d7a7c99fde8550ed18b5bdb4c01e0
Christelle <christelle.rolin@outlook.fr>
2025-09-22 14:32:26 +0200

    popup msg
    creation popup generique

---
+++ src/app/components/popupMsg/popupMsg.component.ts
@@ -0,0 +1,29 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-popup-message',
+  standalone: true,
+  templateUrl: './popupMsg.component.html',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  styleUrls: ['./popupMsg.component.scss']
+})
+export class PopupMessageComponent {
+  @Input() titre: string = 'INFORMATION';
+  @Input() messages: string[] = [];
+  @Input() reponse: boolean = false;
+
+  @Output() fermer = new EventEmitter<void>();
+  @Output() valider = new EventEmitter<void>();
+
+  close() {
+    this.fermer.emit();
+  }
+
+  confirm() {
+    this.valider.emit();
+  }
+}
