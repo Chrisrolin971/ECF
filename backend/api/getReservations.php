5009fa2eee2d714ee5e811e0e93d12d4770a93e1
Christelle <christelle.rolin@outlook.fr>
2026-01-29 10:41:35 +0100

    test 2 lier bdd

--- backend/api/getReservations.php
+++ backend/api/getReservations.php
@@ -1,6 +1,8 @@
 ﻿<?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:4200');
+$allowedOrigins = [ "https://projet-cinephoria.fr", "https://home-5019114412.app-ionos.space" ];
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']); }
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
